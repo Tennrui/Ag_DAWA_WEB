14c31c5e4896246cfade59e1230d87e102c6dfce
Tennrui <89821291+Tennrui@users.noreply.github.com>
2022-03-23 18:10:13 -0600

    Ag_WEB
    subida de AG_ WEB

---
+++ Consultas/Obtener.php
@@ -0,0 +1,28 @@
+<?php
+$id = $_POST['id'];
+
+
+$server = 'localhost';
+$user = 'id18660761_user1';
+$pass = 'MWjB8a8T$IWk!dXI';
+$basedatos = 'id18660761_ag_autos';
+$conn = mysqli_connect($server, $user, $pass) or die("Error: No se pudo conectar");
+//establecer conexion con BD
+
+
+
+mysqli_select_db($conn, $basedatos) or die("Error no se ha encontrado la base de datos");
+
+$query = "SELECT num_serie,costo_pieza FROM piezas WHERE nombre_pieza = '$id' ";
+
+
+$resultado = mysqli_query($conn,$query);
+if(!$resultado) die ("Error: no se pudo eliminar al empleado");
+
+$num_registros = mysqli_num_rows($resultado);
+$registro = mysqli_fetch_row($resultado);
+//echo "Cantidad $registro[0]";
+//echo "Costo $registro[1]";
+mysqli_close($conn);
+echo json_encode($registro);
+?>
